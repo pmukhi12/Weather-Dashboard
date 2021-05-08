5fa1c1afcfdb8a6f9c5e916c14b44ae763ff751c
Prav Mukhi <praveshmukhi@gmail.com>
2021-05-08 17:21:01 -0400

    Updated API data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,59 @@
 
 
-
 // Variable for City-State
 //Upd
-var searchResult = "New York"
-var apiKey = "30148a43276dc2554a57267ec878f1f9"
-var cityUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchResult + "&appid=30148a43276dc2554a57267ec878f1f9"
-va
+var searchBtn = document.getElementById('search');
+
+function getSearchVal () {
+    var searchValue = document.getElementById('city-input').value;
+    console.log({searchValue})
+    if (searchValue) {
+        getGeoLocation(searchValue);
+        document.getElementById('city-input').value = ''
+    }
+    
+}
 
 
 // Fetch the location URL, Return the Object in Json Format
 
-function processFetch(response){
-    return response.json()
-}) 
+function getGeoLocation(searchResult) {
+    var apiKey = "30148a43276dc2554a57267ec878f1f9"
+    var cityUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchResult + "&appid="+ apiKey
+    fetch(cityUrl)
+        .then(function (response) {
+            console.log(response)
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data);
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            getWeatherByLoc(lat,lon);
+        })
+}
 
 // Store the data of the first element and pull the latitude and longitude for the Search Location
-function parseLocation((data){
+function parseLocation(data){
     var lat = data[0].lat
     var lon = data[0].lon
     return [lat, lon]
-})
-
-var 
-
-function getWeatherByLoc(response) {
-    fetch()
 }
 
+function getWeatherByLoc(lat,lon) {
+    var apiKey = "30148a43276dc2554a57267ec878f1f9"
+    var locationUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial"
+    fetch(locationUrl) 
+    .then(function (response){
+        console.log(response);
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data)
+    })
 
-fetch (cityUrl)
-    .then(processFetch)
-    .then(parseLocation)
+}
+
+// getGeoLocation('Boston')
+searchBtn.addEventListener('click',getSearchVal)
+
