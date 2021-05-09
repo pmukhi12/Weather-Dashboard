d0a6a645d269b0a5f4595a369f94968e1ce98f01
Prav Mukhi <praveshmukhi@gmail.com>
2021-05-08 20:39:24 -0400

    Updated index.html, documented and added all relevant elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 
+var citiesStored = JSON.parse(localStorage.getItem('cities')) || [];
+
 
 // Variable for City-State
 //Upd
@@ -21,24 +23,22 @@ function getGeoLocation(searchResult) {
     var apiKey = "30148a43276dc2554a57267ec878f1f9"
     var cityUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchResult + "&appid="+ apiKey
     fetch(cityUrl)
-        .then(function (response) {
-            console.log(response)
-            return response.json()
-        })
-        .then(function (data) {
-            console.log(data);
-            var lat = data[0].lat;
-            var lon = data[0].lon;
-            getWeatherByLoc(lat,lon);
+    .then(function (response) {
+        if (!response.ok) {
+            throw Error(response.statusText + "-" + cityUrl)
+        }
+        return response.json()
+    })
+    .then(function (data) {
+        console.log(data);
+        citiesStored.push(searchResult)
+        localStorage.setItem('cities',JSON.stringify(citiesStored))
+        var lat = data[0].lat;
+        var lon = data[0].lon;
+        getWeatherByLoc(lat,lon);
         })
 }
 
-// Store the data of the first element and pull the latitude and longitude for the Search Location
-function parseLocation(data){
-    var lat = data[0].lat
-    var lon = data[0].lon
-    return [lat, lon]
-}
 
 function getWeatherByLoc(lat,lon) {
     var apiKey = "30148a43276dc2554a57267ec878f1f9"
@@ -51,9 +51,8 @@ function getWeatherByLoc(lat,lon) {
     .then(function(data) {
         console.log(data)
     })
-
 }
 
-// getGeoLocation('Boston')
+
 searchBtn.addEventListener('click',getSearchVal)
 
